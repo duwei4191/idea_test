0c826146c26605990ee60312605f3fe253311d87
杜伟 <duwei_420@163.com>
2019-10-17 10:16:31 +0800

    第一次提交

---
+++ src/User.java
@@ -0,0 +1,51 @@
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) &&
+                Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    public static void main(String[] args) {
+       User user =  new User();
+        System.out.println(user);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
+
